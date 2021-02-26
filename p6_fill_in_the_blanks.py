5a18eb0f9b0713e224cfbb2e603db110c77fc26c
Phillip Flores <phillipf@uci.edu>
2021-02-26 15:01:07 -0800

    Solution to fill in the list. (1) if None set the previous index (2) if there multiple Nones in succession     it won't be a problem as the previous     is already replaced with the previous     indexed value.

--- p6_fill_in_the_blanks.py
+++ p6_fill_in_the_blanks.py
@@ -1,7 +1,25 @@
 # Given an array containing None values fill in the None values with most recent
 # non None value in the array
 
-array1 = [1, None, 2, 3, None, None, 5, None]
-
 import unittest
 
+array1 = [1, None, 2, 3, None, None, 5, None]
+
+
+def fill_in_the_blanks(c: list) -> list:
+    for i in range(len(c)):
+        if c[i] is None:
+            c[i] = c[i-1]
+    return c
+
+
+print(fill_in_the_blanks([1, None, 2, 3, None, None, 5, None]))
+
+
+class TestingBlock(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(fill_in_the_blanks([1, None, 2, 3, None, None, 5, None]),[1, 1, 2, 3, 3, 3, 5, 5])
+
+
+if __name__ == "__main__":
+    unittest.main()
